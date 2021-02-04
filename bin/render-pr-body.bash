6df9167dd92e3623eaaaaeda0b8b668e699c0ebb
Alain Schlesser <alain.schlesser@gmail.com>
2021-02-04 11:09:46 +0000

    Merge 660bd26a122440dcaf76a00c3afb5e7e45020241 into 12a595ad4a97b8ed5efbf9b1f81f619688752d0b

---
+++ bin/render-pr-body.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+get_files() {
+    git ls-files "$1" --exclude-per-directory=.gitignore --exclude-from=.git/info/exclude --exclude-standard
+}
+
+echo "This is an automated pull-request containing the following changes:"
+
+if [[ $(get_files -o) ]]; then
+    echo -e "\n### Files added\n"
+
+    for FILE in $(get_files -o); do
+        echo "- \`$FILE\`"
+    done
+fi
+
+if [[ $(get_files -d) ]]; then
+    echo -e "\n### Files deleted\n"
+
+    for FILE in $(get_files -d); do
+        echo "- \`$FILE\`"
+    done
+fi
+
+if [[ $(get_files -m) ]]; then
+    echo -e "\n### Files modified\n"
+
+    for FILE in $(get_files -m); do
+        echo "- \`$FILE\`"
+    done
+fi
