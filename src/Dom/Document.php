bbd992502f8894144de9b4597d9226b95219e175
Weston Ruter <westonruter@google.com>
2021-02-10 21:55:03 -0800

    Fix phpcs issue

--- src/Dom/Document.php
+++ src/Dom/Document.php
@@ -1316,7 +1316,7 @@ final class Document extends DOMDocument
                     // entities. In the case of a URL value like '/foo/?bar=1&baz=2' the result is a warning for an
                     // unterminated entity reference "baz". When the attribute value is updated via setAttribute() this
                     // same problem does not occur, so that is why the following is used.
-                    $attribute->parentNode->setAttribute( $attribute->nodeName, $value );
+                    $attribute->parentNode->setAttribute($attribute->nodeName, $value);
 
                     $this->mustacheTagsReplaced = true;
                 }
