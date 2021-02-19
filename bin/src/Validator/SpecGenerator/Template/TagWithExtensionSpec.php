4652f7fcb24fcc3dfc836a76b6f21c15a640945f
Alain Schlesser <alain.schlesser@gmail.com>
2021-02-19 10:18:11 +0000

    Add missing interface method declarations for extension spec

--- bin/src/Validator/SpecGenerator/Template/TagWithExtensionSpec.php
+++ bin/src/Validator/SpecGenerator/Template/TagWithExtensionSpec.php
@@ -4,6 +4,12 @@ namespace AmpProject\Tooling\Validator\SpecGenerator\Template;
 
 interface TagWithExtensionSpec
 {
+    /**
+     * Get the name of the extension.
+     *
+     * @return string Extension name.
+     */
+    public function getExtensionName();
 
     /**
      * Get the latest available version of the extension.
@@ -11,4 +17,11 @@ interface TagWithExtensionSpec
      * @return string Latest available version.
      */
     public function getLatestVersion();
+
+    /**
+     * Get the type of the extension.
+     *
+     * @return string Extension type.
+     */
+    public function getExtensionType();
 }
--- src/Validator/Spec/TagWithExtensionSpec.php
+++ src/Validator/Spec/TagWithExtensionSpec.php
@@ -10,9 +10,23 @@ namespace AmpProject\Validator\Spec;
 interface TagWithExtensionSpec
 {
     /**
+     * Get the name of the extension.
+     *
+     * @return string Extension name.
+     */
+    public function getExtensionName();
+
+    /**
      * Get the latest available version of the extension.
      *
      * @return string Latest available version.
      */
     public function getLatestVersion();
+
+    /**
+     * Get the type of the extension.
+     *
+     * @return string Extension type.
+     */
+    public function getExtensionType();
 }
