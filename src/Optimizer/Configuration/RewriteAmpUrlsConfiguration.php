f0d690131ddace2cc0aa964b31b98a87346f8d7a
Alain Schlesser <alain.schlesser@gmail.com>
2021-04-01 09:38:55 +0100

    Add missing types to configuration property declarations

--- src/Optimizer/Configuration/RewriteAmpUrlsConfiguration.php
+++ src/Optimizer/Configuration/RewriteAmpUrlsConfiguration.php
@@ -8,17 +8,27 @@ use AmpProject\Optimizer\Exception\InvalidConfigurationValue;
 /**
  * Configuration for the RewriteAmpUrls transformer.
  *
- * @property $ampUrlPrefix      Specifies an URL prefix for AMP runtime URLs.
- * @property $ampRuntimeVersion Specifies a specific version of the AMP runtime.
- * @property $lts               Use long-term stable URLs.
- * @property $rtv               Append the runtime version to the rewritten URLs.
- * @property $geoApiUrl         Specifies amp-geo API URL to use as a fallback.
- * @property $experimentEsm     Whether to use ES modules for loading the AMP runtime and components.
+ * @property string $ampRuntimeVersion Specifies a specific version of the AMP runtime.
+ * @property string $ampUrlPrefix      Specifies an URL prefix for AMP runtime URLs.
+ * @property bool   $experimentEsm     Whether to use ES modules for loading the AMP runtime and components.
+ * @property string $geoApiUrl         Specifies amp-geo API URL to use as a fallback.
+ * @property bool   $lts               Use long-term stable URLs.
+ * @property bool   $rtv               Append the runtime version to the rewritten URLs.
  *
  * @package ampproject/amp-toolbox
  */
 final class RewriteAmpUrlsConfiguration extends BaseTransformerConfiguration
 {
+
+    /**
+     * Specifies a specific version of the AMP runtime.
+     *
+     * For example: `ampRuntimeVersion: "001515617716922"` will result in AMP runtime URLs being re-written from
+     * `https://cdn.ampproject.org/v0.js` to `https://cdn.ampproject.org/rtv/001515617716922/v0.js`.
+     *
+     * @var string
+     */
+    const AMP_RUNTIME_VERSION = 'ampRuntimeVersion';
 
     /**
      * Specifies an URL prefix for AMP runtime URLs.
@@ -31,14 +41,22 @@ final class RewriteAmpUrlsConfiguration extends BaseTransformerConfiguration
     const AMP_URL_PREFIX = 'ampUrlPrefix';
 
     /**
-     * Specifies a specific version of the AMP runtime.
+     * Whether to use ES modules for loading the AMP runtime and components.
      *
-     * For example: `ampRuntimeVersion: "001515617716922"` will result in AMP runtime URLs being re-written from
-     * `https://cdn.ampproject.org/v0.js` to `https://cdn.ampproject.org/rtv/001515617716922/v0.js`.
+     * ==> EXPERIMENTAL <==
      *
      * @var string
      */
-    const AMP_RUNTIME_VERSION = 'ampRuntimeVersion';
+    const EXPERIMENT_ESM = 'experimentEsm';
+
+    /**
+     * Specifies amp-geo API URL to use as a fallback when `amp-geo-0.1.js` is served unpatched.
+     *
+     * This is used when `{{AMP_ISO_COUNTRY_HOTPATCH}}` is not replaced dynamically.
+     *
+     * @var string
+     */
+    const GEO_API_URL = 'geoApiUrl';
 
     /**
      * Use long-term stable URLs.
@@ -63,24 +81,6 @@ final class RewriteAmpUrlsConfiguration extends BaseTransformerConfiguration
     const RTV = 'rtv';
 
     /**
-     * Specifies amp-geo API URL to use as a fallback when `amp-geo-0.1.js` is served unpatched.
-     *
-     * This is used when `{{AMP_ISO_COUNTRY_HOTPATCH}}` is not replaced dynamically.
-     *
-     * @var string
-     */
-    const GEO_API_URL = 'geoApiUrl';
-
-    /**
-     * Whether to use ES modules for loading the AMP runtime and components.
-     *
-     * ==> EXPERIMENTAL <==
-     *
-     * @var string
-     */
-    const EXPERIMENT_ESM = 'experimentEsm';
-
-    /**
      * Get the associative array of allowed keys and their respective default
      * values.
      *
@@ -93,12 +93,12 @@ final class RewriteAmpUrlsConfiguration extends BaseTransformerConfiguration
     protected function getAllowedKeys()
     {
         return [
-            self::AMP_URL_PREFIX      => Amp::CACHE_HOST,
             self::AMP_RUNTIME_VERSION => '',
+            self::AMP_URL_PREFIX      => Amp::CACHE_HOST,
+            self::EXPERIMENT_ESM      => false,
+            self::GEO_API_URL         => '',
             self::LTS                 => false,
             self::RTV                 => false,
-            self::GEO_API_URL         => '',
-            self::EXPERIMENT_ESM      => false,
         ];
     }
 
@@ -112,6 +112,18 @@ final class RewriteAmpUrlsConfiguration extends BaseTransformerConfiguration
     protected function validate($key, $value)
     {
         switch ($key) {
+            case self::AMP_RUNTIME_VERSION:
+                if (! is_string($value)) {
+                    throw InvalidConfigurationValue::forInvalidSubValueType(
+                        self::class,
+                        self::AMP_RUNTIME_VERSION,
+                        'string',
+                        gettype($value)
+                    );
+                }
+                $value = trim($value);
+                break;
+
             case self::AMP_URL_PREFIX:
                 if (! is_string($value)) {
                     throw InvalidConfigurationValue::forInvalidSubValueType(
@@ -124,11 +136,22 @@ final class RewriteAmpUrlsConfiguration extends BaseTransformerConfiguration
                 $value = trim($value);
                 break;
 
-            case self::AMP_RUNTIME_VERSION:
+            case self::EXPERIMENT_ESM:
+                if (! is_bool($value)) {
+                    throw InvalidConfigurationValue::forInvalidSubValueType(
+                        self::class,
+                        self::EXPERIMENT_ESM,
+                        'boolean',
+                        gettype($value)
+                    );
+                }
+                break;
+
+            case self::GEO_API_URL:
                 if (! is_string($value)) {
                     throw InvalidConfigurationValue::forInvalidSubValueType(
                         self::class,
-                        self::AMP_RUNTIME_VERSION,
+                        self::GEO_API_URL,
                         'string',
                         gettype($value)
                     );
@@ -152,29 +175,6 @@ final class RewriteAmpUrlsConfiguration extends BaseTransformerConfiguration
                     throw InvalidConfigurationValue::forInvalidSubValueType(
                         self::class,
                         self::RTV,
-                        'boolean',
-                        gettype($value)
-                    );
-                }
-                break;
-
-            case self::GEO_API_URL:
-                if (! is_string($value)) {
-                    throw InvalidConfigurationValue::forInvalidSubValueType(
-                        self::class,
-                        self::GEO_API_URL,
-                        'string',
-                        gettype($value)
-                    );
-                }
-                $value = trim($value);
-                break;
-
-            case self::EXPERIMENT_ESM:
-                if (! is_bool($value)) {
-                    throw InvalidConfigurationValue::forInvalidSubValueType(
-                        self::class,
-                        self::EXPERIMENT_ESM,
                         'boolean',
                         gettype($value)
                     );
