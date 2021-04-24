fdec2a436d3cd70feb84f4e635b5c48f7f7b97a5
Alain Schlesser <alain.schlesser@gmail.com>
2021-04-24 18:55:50 +0100

    Add missing test scenarios for Url.php

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -17,17 +17,22 @@ class UrlTest extends TestCase
     public function dataParsing()
     {
         return [
-            ['', [], '//localhost/'],
-            ['https://localhost', ['scheme' => 'https', 'host' => 'localhost'], 'https://localhost/'],
-            ['//example.com', ['host' => 'example.com'], '//example.com/'],
-            ['/', ['path' => '/'], '//localhost/'],
-            ['//example.com:2222', ['host' => 'example.com', 'port' => '2222'], '//example.com:2222/'],
-            ['//john@example.com', ['user' => 'john', 'host' => 'example.com'], '//john@example.com/'],
-            ['//john:123@example.com', ['user' => 'john', 'pass' => '123', 'host' => 'example.com'], '//john:123@example.com/'],
-            ['https://example.com', ['scheme' => 'https', 'host' => 'example.com'], 'https://example.com/'],
-            ['//example.com/demo/download', ['host' => 'example.com', 'path' => '/demo/download'], '//example.com/demo/download'],
-            ['//example.com?filter=true', ['host' => 'example.com', 'query' => 'filter=true'], '//example.com/?filter=true'],
-            ['//example.com#anchor', ['host' => 'example.com', 'fragment' => 'anchor'], '//example.com/#anchor'],
+            ['', null, [], '//localhost/'],
+            ['https://localhost', null, ['scheme' => 'https', 'host' => 'localhost'], 'https://localhost/'],
+            ['//example.com', null, ['host' => 'example.com'], '//example.com/'],
+            ['/', null, ['path' => '/'], '//localhost/'],
+            ['//example.com:2222', null, ['host' => 'example.com', 'port' => '2222'], '//example.com:2222/'],
+            ['//john@example.com', null, ['user' => 'john', 'host' => 'example.com'], '//john@example.com/'],
+            ['//john:123@example.com', null, ['user' => 'john', 'pass' => '123', 'host' => 'example.com'], '//john:123@example.com/'],
+            ['https://example.com', null, ['scheme' => 'https', 'host' => 'example.com'], 'https://example.com/'],
+            ['//example.com/demo/download', null, ['host' => 'example.com', 'path' => '/demo/download'], '//example.com/demo/download'],
+            ['//example.com?filter=true', null, ['host' => 'example.com', 'query' => 'filter=true'], '//example.com/?filter=true'],
+            ['//example.com#anchor', null, ['host' => 'example.com', 'fragment' => 'anchor'], '//example.com/#anchor'],
+            ['//example.com/demo/download', new Url('//example.com/demo/'), ['host' => 'example.com', 'path' => '/demo/download'], '//example.com/demo/download'],
+            ['download', new Url('//example.com/demo/'), ['host' => 'example.com', 'path' => '/demo/download'], '//example.com/demo/download'],
+            ['../download', new Url('//example.com/demo/'), ['host' => 'example.com', 'path' => '/download'], '//example.com/download'],
+            ['./download', new Url('//example.com/demo/'), ['host' => 'example.com', 'path' => '/demo/download'], '//example.com/demo/download'],
+            ['./../demo/.././download', new Url('//example.com/demo/'), ['host' => 'example.com', 'path' => '/download'], '//example.com/download'],
         ];
     }
 
@@ -38,9 +43,9 @@ class UrlTest extends TestCase
      * @covers \AmpProject\Url::toArray()
      * @covers \AmpProject\Url::toString()
      */
-    public function testParsing($urlString, $expectedArray, $expectedString)
+    public function testParsing($urlString, $baseUrl, $expectedArray, $expectedString)
     {
-        $url = new Url($urlString);
+        $url = new Url($urlString, $baseUrl);
         $this->assertEquals($expectedArray, $url->toArray(true));
         $this->assertEquals($expectedString, Url::toString($url->toArray()));
         $this->assertEquals($expectedString, Url::toString($url->toArray(true)));
@@ -90,5 +95,35 @@ class UrlTest extends TestCase
     public function testIsValidNonDataUrl($src, $expected)
     {
         $this->assertEquals($expected, (new Url($src))->isValidNonDataUrl());
+    }
+
+    public function testThrowsErrorOnInvalidUrlPart()
+    {
+        $url = new Url('//example.com');
+
+        // Testing the return value _after_ the trigger_error() is not easily done in PHPUnit,
+        // so we add our own error assertion here to test this return value.
+        $errors = [];
+        set_error_handler(
+            static function ($errno, $errstr, $errfile, $errline, $errcontext = []) use (&$errors) {
+                $errors[] = compact('errno', 'errstr', 'errfile', 'errline', 'errcontext');
+            }
+        );
+
+        $this->assertNull($url->nonsense);
+        $this->assertError($errors, 'Undefined property: AmpProject\Url::nonsense', E_USER_NOTICE);
+    }
+
+    private function assertError($errors, $errstr, $errno)
+    {
+        foreach ($errors as $error) {
+            if ($error['errstr'] === $errstr && $error['errno'] === $errno) {
+                return;
+            }
+        }
+
+        $this->fail(
+            "Expected error with level {$errno} and message '{$errstr}' was not triggered."
+        );
     }
 }
