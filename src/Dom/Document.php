a1103ee4cbc16e0124672279be60dcd1b064dcfc
Alain Schlesser <alain.schlesser@gmail.com>
2021-05-11 17:13:46 +0100

    Rephrase recommendation

--- src/Dom/Document.php
+++ src/Dom/Document.php
@@ -350,7 +350,8 @@ final class Document extends DOMDocument
     /**
      * Named constructor to provide convenient way of transforming HTML into DOM.
      *
-     * Due to slow automatic encoding detection, we recommend passing it ether in meta charset or in $options.
+     * Due to slow automatic encoding detection, it is recommended to provide an explicit
+     * charset either via a <meta charset> tag or via $options.
      *
      * @param string       $html    HTML to turn into a DOM.
      * @param array|string $options Optional. Array of options to configure the document. Used as encoding if a string
@@ -379,7 +380,9 @@ final class Document extends DOMDocument
      * Named constructor to provide convenient way of transforming a HTML fragment into DOM.
      *
      * The difference to Document::fromHtml() is that fragments are not normalized as to their structure.
-     * Due to slow automatic encoding detection, we recommend passing it in $options.
+     *
+     * Due to slow automatic encoding detection, it is recommended to pass in an explicit
+     * charset via $options.
      *
      * @param string       $html    HTML to turn into a DOM.
      * @param array|string $options Optional. Array of options to configure the document. Used as encoding if a string
