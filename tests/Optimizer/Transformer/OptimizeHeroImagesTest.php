16b9f6689b66031fd09d512955fb1bae36236115
thelovekesh <lovekesh.kumar@rtcamp.com>
2023-10-25 02:01:27 +0530

    Update `OptimizeHeroImages::generateImg()` test cases

--- tests/Optimizer/Transformer/OptimizeHeroImagesTest.php
+++ tests/Optimizer/Transformer/OptimizeHeroImagesTest.php
@@ -351,16 +351,18 @@ final class OptimizeHeroImagesTest extends TestCase
                 ),
             ],
 
-            'automatically ssr img element with high fetchpriority attribute from noscript fallback img' => [
+            'automatically ssr img element with high fetchpriority attribute from noscript fallback img even when not a hero image' => [
                 $input(
-                    '<amp-img width="500" height="400" src="/img1.png"><noscript><img src="/img1.png" width="500" height="400" fetchpriority="high"></noscript></amp-img>'
-                    .  '<amp-img width="500" height="400" src="/img2.png"><noscript><img src="/img2.png" width="500" height="400"></noscript></amp-img>'
-                    .  '<amp-img width="500" height="400" src="/img3.png"><noscript><img src="/img3.png" width="500" height="400" fetchpriority="high"></noscript></amp-img>'
+                    '<amp-img width="500" height="400" src="/img1.png"></amp-img>'
+                    . '<p>Hello World 1</p>'
+                    . '<p>Hello World 2</p>'
+                    .  '<amp-img width="500" height="400" src="/img2.png"><noscript><img src="/img2.png" width="500" height="400" fetchpriority="high"></noscript></amp-img>'
                 ),
                 $output(
-                    '<amp-img data-hero width="500" height="400" src="/img1.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" fetchpriority="high" src="/img1.png"></amp-img>'
-                    . '<amp-img width="500" height="400" src="/img2.png"><noscript><img src="/img2.png" width="500" height="400"></noscript></amp-img>'
-                    . '<amp-img data-hero width="500" height="400" src="/img3.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" fetchpriority="high" src="/img3.png"></amp-img>'
+                    '<amp-img width="500" height="400" src="/img1.png"></amp-img>'
+                    . '<p>Hello World 1</p>'
+                    . '<p>Hello World 2</p>'
+                    . '<amp-img data-hero width="500" height="400" src="/img2.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" fetchpriority="high" src="/img2.png"></amp-img>'
                 ),
             ],
         ];
