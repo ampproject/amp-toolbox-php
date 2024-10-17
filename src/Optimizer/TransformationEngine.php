646aa2a75a8607070beaf28cbd3471fea7d8c4c0
Pascal Birchler <pascalb@google.com>
2024-10-17 14:13:17 +0200

    Update param type

--- src/Optimizer/TransformationEngine.php
+++ src/Optimizer/TransformationEngine.php
@@ -51,7 +51,7 @@ final class TransformationEngine
      * @param Configuration|null    $configuration Optional. Configuration data to use for setting up the transformers.
      * @param RemoteGetRequest|null $remoteRequest Optional. Transport to use for remote requests. Defaults to the
      *                                             CurlRemoteGetRequest implementation shipped with the library.
-     * @param Spec                  $spec          Optional. Validator spec instance to use.
+     * @param Spec|null             $spec          Optional. Validator spec instance to use.
      */
     public function __construct(
         ?Configuration $configuration = null,
